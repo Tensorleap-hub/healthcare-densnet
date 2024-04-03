22315cfca62806033f26467f36af2342900deca4
Chen Rothschild <chen.rothschild@tensorleap.ai>
2024-04-03 15:30:39 +0300

    add models to custom test

--- leap_custom_test.py
+++ leap_custom_test.py
@@ -12,18 +12,18 @@ def check_custom_integration():
     val = preprocess[1]
     test = preprocess[2]
     dir_path = os.path.dirname(os.path.abspath(__file__))
-    model_path = 'model/DenseNet201.h5'
-    cnn = tf.keras.models.load_model(os.path.join(dir_path, model_path))
+    model_path = 'model/densnet.h5'
+    model = tf.keras.models.load_model(os.path.join(dir_path, model_path))
 
     set = train
-    for idx in range(20):
+    for idx in range(10):
         # get input and gt
         image_input = input_encoder(idx, set)
         concat = np.expand_dims(image_input, axis=0)
         gt = gt_encoder(idx, set)
         y_true = tf.convert_to_tensor(np.expand_dims(gt, axis=0))
 
-        y_pred = cnn([concat])
+        y_pred = model([concat])
 
         # get loss
         ls = CategoricalCrossentropy()(y_true, y_pred)
@@ -31,6 +31,7 @@ def check_custom_integration():
         # get meatdata
         label = metadata_gt_label(idx, set)
         label_name = metadata_gt_name(idx, set)
+        print(f"label_name: {label_name}")
         black_pixels = metadata_count_black_pixels(idx, set)
         pixels_count = metadata_pixels_count(idx, set)
 
@@ -46,15 +47,17 @@ def check_custom_integration():
         top_50_values_str = [str(number) for number in top_50_values.tolist()]
         top_50_indices_str = [str(number) for number in top_50_indices.tolist()]
 
-        # Plot the histogram
-        x_values = np.arange(256)
-        plt.figure(figsize=(8, 6))
-        plt.bar(x_values, histogram, color='gray', alpha=0.7)
-        plt.xlabel('Pixel Value')
-        plt.ylabel('Frequency')
-        plt.title('Grayscale Histogram')
-        plt.grid(axis='y', linestyle='--', alpha=0.7)
-        plt.show()
+        # # Plot the histogram
+        # x_values = np.arange(256)
+        # plt.figure(figsize=(8, 6))
+        # plt.bar(x_values, histogram, color='gray', alpha=0.7)
+        # plt.xlabel('Pixel Value')
+        # plt.ylabel('Frequency')
+        # plt.title('Grayscale Histogram')
+        # plt.grid(axis='y', linestyle='--', alpha=0.7)
+        # plt.show()
+
+        print(f"idx {idx} successfully finish")
 
 
 if __name__ == "__main__":
