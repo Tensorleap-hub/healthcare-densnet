dca23424df89543727c36496f5169c11616fc5d0
Idan <46688952+tip3x@users.noreply.github.com>
2025-11-02 15:52:14 +0200

    code mapping (#5)
    * code mapping
    
    * clean version
    
    ---------
    
    Co-authored-by: Idan <idan@idan.com>
    Co-authored-by: yamtawachi <yam.tawachi@tensorleap.ai>

--- leap_binder.py
+++ leap_binder.py
@@ -1,5 +1,4 @@
-from typing import List, Dict
-import numpy as np
+from code_loader.default_metrics import categorical_crossentropy
 from numpy import ndarray
 from healthcare.config import CONFIG
 from healthcare.data.preprocess import load_images, model_name
@@ -8,16 +7,15 @@ from healthcare.utils.general_utils import load_image, flat_image
 from healthcare.utils.visuelizers import pixels_histogram
 import cv2
 
-# Tensorleap imports
-from code_loader import leap_binder
-from code_loader.contract.datasetclasses import PreprocessResponse
-from code_loader.contract.visualizer_classes import LeapHorizontalBar
+
+from code_loader.inner_leap_binder.leapbinder_decorators import *
 
 if model_name == 'DenseNet201':
     labels = CONFIG['LABELS_DenseNet201']
 else:
     labels = CONFIG['LABELS']
 
+@tensorleap_preprocess()
 def preprocess_func() -> List[PreprocessResponse]:
     train_X, train_Y = load_images('train')
     val_X, val_Y = load_images('val')
@@ -42,7 +40,11 @@ def input_encoder_cnn(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
     img = grayscale_image_3d / 255
     return img.astype('float32')
 
+@tensorleap_input_encoder('image')
 def input_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
+    if model_name == 'cnn':
+        return input_encoder_cnn(idx, preprocess)
+
     cloud_path = preprocess.data['images'][idx]
     fpath = _download(str(cloud_path))
     img = load_image(fpath)
@@ -51,10 +53,12 @@ def input_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
 
 
 
+@tensorleap_gt_encoder("classes")
 def gt_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
-    return preprocess.data['labels'][idx]
+    return np.array(preprocess.data['labels'][idx]).astype(np.float32)
 
 
+@tensorleap_metadata("gt_label")
 def metadata_gt_label(idx: int, preprocess: PreprocessResponse) -> int:
     one_hot_digit = np.array(gt_encoder(idx, preprocess))
     digit = one_hot_digit.argmax()
@@ -73,7 +77,11 @@ def metadata_gt_name_DenseNet201(idx: int, preprocess: PreprocessResponse) -> st
     else:
         raise ValueError("Invalid label value")
 
+@tensorleap_metadata("gt_name")
 def metadata_gt_name(idx: int, preprocess: PreprocessResponse) -> str:
+    if model_name == 'DenseNet201':
+        return metadata_gt_name_DenseNet201(idx, preprocess)
+
     digit = gt_encoder(idx, preprocess)
     if digit[0] == 1:
         return 'NORMAL'
@@ -83,6 +91,7 @@ def metadata_gt_name(idx: int, preprocess: PreprocessResponse) -> str:
         raise ValueError("Invalid label value")
 
 
+@tensorleap_metadata("number_of_black_pixels")
 def metadata_count_black_pixels(idx: int, preprocess: PreprocessResponse) -> ndarray:
     cloud_path = preprocess.data['images'][idx]
     fpath = _download(str(cloud_path))
@@ -93,50 +102,43 @@ def metadata_count_black_pixels(idx: int, preprocess: PreprocessResponse) -> nda
     return black_pixel_count
 
 
+@tensorleap_metadata("pixels_count")
 def metadata_pixels_count(idx: int, preprocess: PreprocessResponse) -> Dict:
     cloud_path = preprocess.data['images'][idx]
     fpath = _download(str(cloud_path))
     img = load_image(fpath)
     flattened_image = flat_image(img)
-    pixel_counts = {key: 0 for key in range(256)}
+    pixel_counts = {str(key): 0 for key in range(256)}
 
     for pixel_value in flattened_image:
         pixel_value_int = int(pixel_value)
-        pixel_counts[pixel_value_int] += 1
+        pixel_counts[str(pixel_value_int)] += 1
 
     return pixel_counts
 
 
+@tensorleap_custom_visualizer('pixels_histogram', LeapHorizontalBar.type)
 def visualizer_pixels_histogram(image: np.ndarray) -> LeapHorizontalBar:
     image = np.squeeze(image)
     top_50_indices, top_50_values_str = pixels_histogram(image)
     return LeapHorizontalBar(np.array(top_50_indices), top_50_values_str)
 
 
+@tensorleap_custom_visualizer('bar_names_visualizer', LeapHorizontalBar.type)
 def bar_visualizer(data: np.ndarray) -> LeapHorizontalBar:
     data = data.astype(np.float32)
     data = np.squeeze(data)
     return LeapHorizontalBar(data, labels)
 
 
-leap_binder.set_preprocess(function=preprocess_func)
+@tensorleap_custom_loss('categorical_crossentropy_loss')
+def categorical_crossentropy_loss(y_true, y_pred):
+    return categorical_crossentropy(y_true, y_pred)
 
-if model_name == 'cnn':
-    leap_binder.set_input(function=input_encoder_cnn, name='image')
-else:
-    leap_binder.set_input(function=input_encoder, name='image')
-leap_binder.set_ground_truth(function=gt_encoder, name='classes')
-leap_binder.add_prediction(name='classes', labels=labels)
-leap_binder.set_metadata(function=metadata_gt_label, name='gt_label')
 
-if model_name == 'DenseNet201':
-    leap_binder.set_metadata(function=metadata_gt_name_DenseNet201, name='gt_name')
-else:
-    leap_binder.set_metadata(function=metadata_gt_name, name='gt_name')
-leap_binder.set_metadata(function=metadata_count_black_pixels, name='number_of_black_pixels')
-leap_binder.set_metadata(function=metadata_pixels_count, name='pixels_count')
-leap_binder.set_visualizer(name='pixels_histogram', function=visualizer_pixels_histogram,
-                           visualizer_type=LeapHorizontalBar.type)
-leap_binder.set_visualizer(name='bar_names_visualizer', function=bar_visualizer,
-                           visualizer_type=LeapHorizontalBar.type)
+
+
+
+
+
 
---
+++ leap_integration.py
@@ -0,0 +1,61 @@
+from code_loader.plot_functions.visualize import visualize
+
+from leap_binder import *
+
+
+if model_name == 'DenseNet201':
+    labels = CONFIG['LABELS_DenseNet201']
+else:
+    labels = CONFIG['LABELS']
+
+
+prediction_type1 = PredictionTypeHandler('classes', labels)
+
+
+
+
+
+@tensorleap_load_model([prediction_type1])
+def load_model():
+    H5_MODEL_PATH = "model/cnn.h5"
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(dir_path, H5_MODEL_PATH)
+    import tensorflow as tf
+    return tf.keras.models.load_model(os.path.join(dir_path, model_path))
+
+
+
+@tensorleap_integration_test()
+def check_custom_integration(idx, preprocess_response: PreprocessResponse):
+    print("Starting custom tests")
+    model = load_model()
+
+
+    image_input = input_encoder(idx, preprocess_response)
+
+    gt = gt_encoder(idx, preprocess_response)
+
+    y_pred = model([image_input])
+
+    ls = categorical_crossentropy_loss(gt, y_pred)
+
+    visualizer_pixels_histogram_ = visualizer_pixels_histogram(image_input)
+    bar_visualizer_gt = bar_visualizer(gt)
+    bar_visualizer_pred = bar_visualizer(y_pred)
+
+    visualize(visualizer_pixels_histogram_)
+    visualize(bar_visualizer_gt)
+    visualize(bar_visualizer_pred)
+
+    label = metadata_gt_label(idx, preprocess_response)
+    black_pixels = metadata_count_black_pixels(idx, preprocess_response)
+    pixels_count = metadata_pixels_count(idx, preprocess_response)
+
+    label_name = metadata_gt_name(idx, preprocess_response)
+
+
+
+if __name__ == '__main__':
+    responses = preprocess_func()
+    train = responses[0]
+    check_custom_integration(0, train)
